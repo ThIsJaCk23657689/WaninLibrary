756eb145c547d056c52d0c36cfc635f85f336d7b
ThIsJaCk <y23657689@gmail.com>
2020-06-23 22:02:01 +0800

    navbar dropdown

--- resources/views/partials/frontend/navbar.blade.php
+++ resources/views/partials/frontend/navbar.blade.php
@@ -30,14 +30,34 @@
                         即時公告/重要消息
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">近期活動</a>
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="activitiesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        近期活動
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="activitiesDropdown">
+                        <a class="dropdown-item" href="#">
+                            <span>近期活動</span>
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <span>主題書單</span>
+                        </a>
+                    </div>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">查詢館藏</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">捐書芳名錄</a>
+                <li class="nav-item dropdown no-arrow">
+                    <a class="nav-link dropdown-toggle" href="#" id="donorDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        捐書芳名錄
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="donorDropdown">
+                        <a class="dropdown-item" href="#">
+                            <span>捐書芳名錄</span>
+                        </a>
+                        <a class="dropdown-item" href="#">
+                            <span>捐贈書籍查詢</span>
+                        </a>
+                    </div>
                 </li>
             </ul>
         </div>
